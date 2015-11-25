3b2a5f4497d24df4fe4d3df06dcde9c02234ccba
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2015-11-26 01:23:56 +0800

    Modify the script for better automation

--- gitlab_user_data.sh
+++ gitlab_user_data.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-apt-get install curl openssh-server ca-certificates postfix
+apt-get update
+echo postfix postfix/mailname string gitlab.nctucs.net | debconf-set-selections
+echo postfix postfix/main_mailer_type select Internet Site | debconf-set-selections
+apt-get install -y curl openssh-server ca-certificates postfix
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
-apt-get install gitlab-ce
+apt-get install -y gitlab-ce
 gitlab-ctl reconfigure
