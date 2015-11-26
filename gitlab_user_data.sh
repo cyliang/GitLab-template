40bec9757bc60ffbfb32bb22f2aa06159bc0163c
Chih-Yung Liang <stu89424@gmail.com>
2015-11-26 14:49:42 +0800

    Automatically set root password.

--- gitlab_user_data.sh
+++ gitlab_user_data.sh
@@ -1,9 +1,28 @@
 #! /bin/bash
 
-apt-get update
-echo postfix postfix/mailname string gitlab.nctucs.net | debconf-set-selections
-echo postfix postfix/main_mailer_type select Internet Site | debconf-set-selections
-apt-get install -y curl openssh-server ca-certificates postfix
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
-apt-get install -y gitlab-ce
-gitlab-ctl reconfigure
+function install-gitlab {
+	# Update repositary
+	apt-get update
+
+	# Install required dependencies
+	echo postfix postfix/mailname string gitlab.nctucs.net | debconf-set-selections
+	echo postfix postfix/main_mailer_type select Internet Site | debconf-set-selections
+	apt-get install -y curl openssh-server ca-certificates postfix
+
+	# Execute installation script & install
+	curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
+	apt-get install -y gitlab-ce
+	gitlab-ctl reconfigure
+}
+
+function set-gitlab-password {
+	gitlab-rails console production <<EOF
+	user = User.where(id: 1).first
+	user.password = '$1'
+	user.password_confirmation = '$1'
+	user.save!
+EOF
+}
+
+install-gitlab
+set-gitlab-password 'gitlab.nctucs.net'
